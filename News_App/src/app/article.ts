d71c2e6fbfc819652be473eef39ad0617968d484
Valiantsin-Kurakevich <valenteen_k@list.ru>
2022-01-24 10:39:43 +0300

    fix:improve code

--- News_App/src/app/article.ts
+++ News_App/src/app/article.ts
@@ -1,3 +1,5 @@
+import { ButtonText } from './article-list';
+
 export interface Article {
   title: string;
   headline: string;
@@ -5,5 +7,7 @@ export interface Article {
   img: string;
   author: string;
   viewCounterArticle: number;
+  isVisible: boolean;
+  buttonText: ButtonText;
   id: number;
 }
--- News_App/src/app/main/main.component.ts
+++ News_App/src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ARTICLES } from '../article-list';
+import { Article } from '../article';
+import { ARTICLES, ButtonText } from '../article-list';
 
 @Component({
   selector: 'app-main',
@@ -10,25 +11,14 @@ export class MainComponent implements OnInit {
   @ViewChild('btnShow', { static: false }) btnShowRef: ElementRef;
 
   public articles = ARTICLES;
-  public isVisible = true;
-  private viewCounterArticle: number = 0;
-  public showArticle(event) {
-    if (event.target.className !== 'article__btn') {
-      return;
-    } else if (event.target.className === 'article__btn') {
-      this.isVisible = !this.isVisible;
-      if (!this.isVisible) {
-        event.target.nextElementSibling.style.display = 'block';
-        event.target.textContent = 'скрыть статью';
-        event.target.nextElementSibling.nextElementSibling.className =
-          'article__picture--active';
-        this.viewCounterArticle++;
-      } else {
-        event.target.nextElementSibling.style.display = 'none';
-        event.target.textContent = 'читать статью полностью';
-        event.target.nextElementSibling.nextElementSibling.className =
-          'article__picture';
-      }
+
+  public toggleArticle(article: Article) {
+    article.isVisible = !article.isVisible;
+    article.buttonText = article.isVisible
+      ? ButtonText.BUTTON_HIDE
+      : ButtonText.BUTTON_SHOW;
+    if (article.isVisible) {
+      article.viewCounterArticle++;
     }
   }
 
